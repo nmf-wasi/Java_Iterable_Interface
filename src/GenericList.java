55bfd39e1ff178f8682804693aec3e82f88fb059
NMF Wasi <wasi7741@gmail.com>
2026-01-16 13:00:48 +0900

    Iterables and Iterators

--- src/GenericList.java
+++ src/GenericList.java
@@ -1,14 +1,48 @@
-import java.util.ArrayList;
+import java.util.Iterator;
 
-public class GenericList<T> {
-//    public T[] items=(T[]) new Object[10];
-    public ArrayList<T> items= (T[])new Object[10];
-    private  int count;
+public class GenericList<T> implements Iterable<T> {
+    //    public T[] items=(T[]) new Object[10];
+    public T[] items = (T[]) new Object[10];
+    private int count;
 
-    public void add(T item){
-        items[count++]=item;
+    // generic lists doesn't allow us to  user iterators by default, however, when we implement iterator class there, we can use it
+    public void add(T item) {
+        items[count++] = item;
     }
-    public T get(int index){
+
+    public T get(int index) {
         return items[index];
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator(this);
+    }
+
+    private class ListIterator implements Iterator<T> {
+        private GenericList<T> list;
+        private int index;
+
+//        this inner class defines how to iterate over the genericList,
+//        so, if we change GenericList to ArrayList or LinkedList, we have to change this inner class methods ony, nothing else
+
+//        since we have used an array in GenericList we will implement iterable on Array for this
+
+        public ListIterator(GenericList<T> list) {
+//            constructor
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index<list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         var list=new GenericList<String>();
-        list.items[0]="a";
-        System.out.println(list.items.length); // when we have a generic Array, we can use this
+        list.add("a");
+        list.add("b");
+        list.add("c");
+//        System.out.println(list.items.length); // when we have a generic Array, we can use this
 //        however, if we use an ArrayList or LinkedList, we can't do this anymoree
 //        becasue ArrayList do not have .length function and it doesnt support [] for accessing elements
+
+        var iterator=list.iterator();
+        while(iterator.hasNext()){ // generic lists doesnt allow us to  user iterators by default, hhowever, when we implement iterator class there, we can use it
+            var current=iterator.next();
+            System.out.println(current);
+        }
+
+        for (var item:list){ //another way to iterate over something
+            System.out.println(item);
+        }
+
     }
 }
+
+
